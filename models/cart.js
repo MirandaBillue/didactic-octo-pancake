75ae1aa9f36c7e6032956b9b81d484921e55ae34
Miranda Billue <110904846+MirandaBillue@users.noreply.github.com>
2022-09-21 09:14:22 -0400

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -14,17 +14,7 @@ const cart = {
        }
    },
 
-   removeFromCart(id = 0, cart) {
-       for(const i = 0; i < cart.items.length; i++) {
-           const item = cart.items[i];
-           if(item.id === id) {
-               cart.items.splice(i, 1);
-               this.calculateTotals(cart);
-           }
-       }
-
-   },
-
+  
   updateCart(ids = [], qtys = [], cart) {
        const map = [];
        const updated = false;
@@ -52,45 +42,6 @@ const cart = {
        }
    },
 
-    inCart(menuID = 0, cart) {
-       const found = false;
-       cart.items.forEach(item => {
-          if(item.id === menuID) {
-              found = true;
-          }
-       });
-       return found;
-   },
-
-    calculateTotals(cart) {
-       cart.totals = 0.00;
-       cart.items.forEach(item => {
-           const price = item.price;
-           const qty = item.qty;
-           const amount = price * qty;
-
-           cart.totals += amount;
-       });
-       this.setFormattedTotals(cart);
-   },
-
-   emptyCart(request) {
-       
-       if(request.session) {
-           request.session.cart.items = [];
-           request.session.cart.totals = 0.00;
-           request.session.cart.formattedTotals = '';
-       }
-
-
-   },
-
-   setFormattedTotals(cart) {
-       const totals = cart.totals;
-       cart.formattedTotals = format.format(totals);
-   }
-
-}
-
+   
 
 module.exports = cart;
